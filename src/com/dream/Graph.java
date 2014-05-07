0ebe93b4e63af5e6ea81cdd1cd5fa6e2406cd171
Gala <galafit@gmail.com>
2014-05-07 21:17:47 +0200

    test class Graph was created

---
+++ src/com/dream/Graph.java
@@ -0,0 +1,52 @@
+package com.dream;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: galafit
+ * Date: 07/05/14
+ * Time: 15:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class Graph {
+    
+    private int frequency;
+    private long startTime;
+    private int zoom;
+    private int startIndex;
+    private boolean isZoomAutomatic;
+    private ArrayList<Integer> inputData = new ArrayList<Integer>();
+
+
+    public Graph(int frequency, long startTime) {
+       this.frequency = frequency;
+       this.startTime = startTime;
+       zoom = 1;
+       startIndex = 0;
+        isZoomAutomatic = true;
+    }
+
+    public void setStartIndex(int index) {
+        startIndex = index;
+
+    }
+
+    public void setZoom (int zoom) {
+        this.zoom = zoom;
+
+    }
+
+    public void addDataElement(int dataElement) {
+        inputData.add(dataElement);
+
+    }
+
+    public void setZoomType(boolean isZoomAutomatic) {
+        isZoomAutomatic = isZoomAutomatic;
+
+    }
+
+
+
+}
